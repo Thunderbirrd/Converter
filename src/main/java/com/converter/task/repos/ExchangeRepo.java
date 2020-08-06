08199121bb0d647800478d62127eb89b8c53ba65
Artem <artemis.gorb.14@gmail.com>
2020-08-06 16:42:53 +0900

    Added methods in ExchangeRepo & ExchangeService

--- src/main/java/com/converter/task/repos/ExchangeRepo.java
+++ src/main/java/com/converter/task/repos/ExchangeRepo.java
@@ -1,10 +1,19 @@
 package com.converter.task.repos;
 
 import com.converter.task.models.Exchange;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface ExchangeRepo extends CrudRepository<Exchange, Integer> {
+    @Query("SELECT exchange FROM Exchange exchange WHERE exchange.date=:date ORDER BY id")
+    List<Exchange> getAllByDate(@Param("date") Date date);
 
+    @Query("SELECT exchange FROM Exchange exchange WHERE exchange.currency1=:currency1 AND" +
+            " exchange.currency2=:currency2 ORDER BY id")
+    List<Exchange> getAllByCurrency1AndCurrency2(@Param("currency1")String currency1, @Param("currency2") String currency2);
 }
--- src/main/java/com/converter/task/services/ExchangeService.java
+++ src/main/java/com/converter/task/services/ExchangeService.java
@@ -1,7 +1,39 @@
 package com.converter.task.services;
 
+import com.converter.task.models.Exchange;
+import com.converter.task.repos.ExchangeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class ExchangeService {
+
+    @Autowired
+    ExchangeRepo exchangeRepo;
+
+    @Transactional
+    public Exchange saveExchange(Exchange exchange){
+        return exchangeRepo.save(exchange);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Exchange> getAllExchangesByDate(Date date){
+        return exchangeRepo.getAllByDate(date);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Exchange> getAllByCurrencies(String currency1, String currency2){
+        return exchangeRepo.getAllByCurrency1AndCurrency2(currency1, currency2);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Exchange> getAllByDateAndCurrencies(Date date, String currency1, String currency2){
+        List<Exchange> listDate = exchangeRepo.getAllByDate(date);
+        List<Exchange> listCurrencies = exchangeRepo.getAllByCurrency1AndCurrency2(currency1, currency2);
+        listDate.retainAll(listCurrencies);
+        return listDate;
+    }
 }
